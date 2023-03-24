9a1b1efec465fd77b6c614e6e8ab536f5c4af8e4
Rafael <rafael.caneca2001@gmail.com>
2023-03-24 00:24:21 -0300

    Teste calc

---
+++ soma.js
@@ -0,0 +1,27 @@
+function Somar() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var soma = parseInt(num1)+parseInt(num2);
+    document.write(soma);
+   }
+   function Dividir() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var soma = parseInt(num1)/parseInt(num2);
+    document.write(soma);
+   }
+   function Subtrair() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var soma = parseInt(num1)-parseInt(num2);
+    document.write(soma);
+   }
+   function Multiplicar() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var soma = parseInt(num1)*parseInt(num2);
+    document.write(soma);
+   }
+
+
+  
